912a0a806093fba7e77e856c6006d684e69a0204
Viv Diwakar <viv@vdiwakar.com>
2017-06-27 14:59:39 +0100

    Added arguments handling for source file and destination directory, per-email buffering working correctly.
    Signed-off-by: vivd <viv@ggh.uk>

--- ParseGmailMboxToMaildir/parsembox.py
+++ ParseGmailMboxToMaildir/parsembox.py
@@ -1,11 +1,41 @@
+import argparse
+import re
 
-a = open("/home/vivd/Work/archives/wk/Mail/sample.mbox", "r")
+parser = argparse.ArgumentParser()
+parser.add_argument('mbox', help='Full path to the mbox file.', type=str)
+parser.add_argument('dest', help='Full path to output the file structure.', type=str)
+args = parser.parse_args()
 
-while True:
-    line = a.readline()
-    if not line:
-        break
-    else:
-        print(line)
+def processBuffer(bufferArray):
+    print(len(bufferArray))
+    print(str(bufferArray) + "\n\n\n")
 
-a.close()
+try:
+    mbox = open(args.mbox, "r")
+
+    buffer = []
+    buffering = False
+
+    rawline = mbox.readline()
+    while rawline:
+        cleaned = rawline.split('\n')[0]
+
+        if buffering == False and re.search('^From .*@xxx ', cleaned):
+            buffer.append(cleaned)
+            buffering = True
+        elif buffering == True and not re.search('^From .*@xxx ', cleaned):
+            buffer.append(cleaned)
+        elif buffering == True and re.search('^From .*@xxx ', cleaned):
+            processBuffer(buffer)
+            buffer.clear()
+            buffer.append(cleaned)
+
+        rawline = mbox.readline()
+
+    processBuffer(buffer)
+
+    mbox.close()
+
+except FileNotFoundError as e:
+    print("Fatal error: " + str(e) + "; exiting.")
+    exit(1)
